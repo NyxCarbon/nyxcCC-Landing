c3e79dff6ed87384142b903226c28e2ea7f8174f
Brittany Salas <brittanysalas90@gmail.com>
2025-07-03 12:11:55 -0700

    NYXC-3 screen formatting

--- frontend/nyxccc-landing/src/components/footer.tsx
+++ frontend/nyxccc-landing/src/components/footer.tsx
@@ -13,10 +13,7 @@ const openExternalLink = (url: string) => {
 const Footer: NextPage<FooterProps> = ({ className = "" }) => {
 
 const onLogoClick = () => {
-  const element = document.getElementById("");
-  if (element) {
-    element.scrollIntoView({ block: "start", behavior: "smooth" });
-  }
+  window.scrollTo({ top: 0, behavior: "smooth" });
 };
 
   return (
@@ -24,6 +21,18 @@ const onLogoClick = () => {
       <section className={styles.footer2}>
         <div className={styles.footerColumn}>
           <div className={styles.footerNav}>
+             <Image
+            className={styles.nyxcLogoWhite11}
+            loading="lazy"
+            width={173.2}
+            height={41}
+            sizes="100vw"
+            alt=""
+            src="/nyxccc-img/nyxc logo white.svg"
+            onClick={onLogoClick}
+            style={{cursor:"pointer"}}            
+            />
+     
             <div className={styles.discordBlock}>
               <Image
                 className={styles.discord59688981Icon}
@@ -35,41 +44,33 @@ const onLogoClick = () => {
                 src="/nyxccc-img/discord_5968898 1.svg"
                 onClick={() => openExternalLink("https://discord.gg/W9KAXfws")}
               />
-              <div className={styles.discordDivider} />
+              <div className={styles.divider} />
             </div>
             <div className={styles.contactBlock}>
               <div className={styles.contactWrapper}>
-                <div className={styles.contact}>
-                  <div className={styles.about}>Newsletter</div>
+                <div className={styles.newsletter}>
+                  <a
+                  href="https://share.hsforms.com/1YXfQ9Z8YRY2fuFz4Kwu3Vgcdvye"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.contact}
+                    >
+                    Newsletter
+                  </a>
                 </div>
               </div>
-              <div className={styles.discordDivider} />
+              <div className={styles.divider} />
             </div>
             <div className={styles.supportBlock}>
               <div className={styles.supportnyxcnetWrapper}>
                 <div className={styles.supportnyxcnet}>support@nyxc.net</div>
               </div>
-              <div className={styles.discordDivider} />
+              <div className={styles.divider} />
             </div>
           </div>
         </div>
-        </section>
-         <div className={styles.nyxcLogoWhite11}>
-                <h2
-                  className={styles.nyxcLogoWhite11}
-                  onClick={onLogoClick}
-                >{``}</h2>
-              </div>
-        <Image
-        className={styles.nyxcLogoWhite11}
-        loading="lazy"
-        width={173.2}
-        height={41}
-        sizes="100vw"
-        alt=""
-        src="/nyxccc-img/nyxc logo white.svg"
-        />
-        <div className={styles.div}>©2025 nyxc Community Loan Fund</div>
+      </section>
+      <div className={styles.div}>©2025 nyxc Community Loan Fund</div>
     </div>
   );
 };
